18530ca2c4705f98e1ff121d8acec68ab139693d
Dennis Moldovan <dennismoldovan32@gmail.com>
2024-10-01 13:52:46 +0300

    chore - changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,19 @@ module.exports = {
         screens: {
           DEFAULT: "1280px",
         },
+        padding: {
+          DEFAULT: "20px"
+        }
       },
+      colors: {
+        "darkBlue":"#4462ff",
+        "lightBlue": "#8c9fff",
+        "orange":"#ff9f2b",
+      }
     },
+    fontFamily: {
+      "poppins": ["Poppins"]
+    }
   },
   plugins: [],
 }
